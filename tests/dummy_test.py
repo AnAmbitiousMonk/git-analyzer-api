eeb16908a7b72687f23e417805eea9b18782f96b
Rishabh Bhardwaj <rishabhb932@gmail.com>
2025-05-15 13:15:09 +0530

    Boilerplate setup

--- tests/dummy_test.py
+++ tests/dummy_test.py
@@ -3,7 +3,8 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_read_root():
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 400
     assert response.json() == {"Hello": "World"}
